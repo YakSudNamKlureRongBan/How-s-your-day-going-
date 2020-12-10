f6c688d1e22b53a7e12f9430afbd0794820872c9
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-12-10 13:38:56 +0700

    Update show_text.php

--- show_text.php
+++ show_text.php
@@ -1,16 +1,23 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Form Post Demo</title>
     </head>
     <body>
     <strong>Diary: </strong>
     <?php
-        echo $_POST['title'];
+        echo $_POST['name'];
     ?>
     <br>
     <?php
-        echo $_POST['text'];
+        echo $_POST['Date'];
+    ?>
+    <br>
+    <?php
+        echo $_POST['Diary'];
+    ?>
+    <br>
+    <?php
+        echo $_POST['Story'];
     ?>
     </body>
 </html>
